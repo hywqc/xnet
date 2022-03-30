a7319c9f882d98e5c4b3190a89b87518bfeece03
wuqinchao <wuqinchao@jiagouyun.com>
2022-03-31 06:16:52 +0800

    444

--- udp/udp.go
+++ udp/udp.go
@@ -1,7 +1,18 @@
 package udp
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/net/html"
+)
 
 func Send() {
 	fmt.Printf("udp send 3")
+	doc, err := html.Parse(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = doc
+
 }
